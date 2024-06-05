10d2b68dc0618afb1c486db07760dfe9b1ac94c7
Gaurav Gupta <gouravgupta840@gmail.com>
2024-06-05 12:39:12 +0530

    refactor syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import "package:flutter/material.dart";
 
-main() {
-  runApp(MyApp());
-}
+void main(List<String> args) => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
