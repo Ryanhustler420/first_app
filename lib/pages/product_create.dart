f2b916c4e879e519278904dcc6b58e5085b77d20
Gaurav Gupta <gouravgupta840@gmail.com>
2024-06-07 15:10:28 +0530

    added more field

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -11,6 +11,8 @@ class ProductCreatePage extends StatefulWidget {
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
   String titleValue = '';
+  double priceValue = 0.0;
+  String descriptionValue = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +22,22 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
           onChanged: (String value) {
             setState(() {
               titleValue = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
             });
           },
         ),
