4ea021dfb6a07f2c544dbb1aae48427b49c2b089
Gaurav Gupta <gouravgupta840@gmail.com>
2024-06-07 15:51:45 +0530

    styled text inputs

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,33 +16,48 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        TextField(
-          maxLines: 4,
-          onChanged: (String value) {
-            setState(() {
-              descriptionValue = value;
-            });
-          },
-        ),
-        TextField(
-          keyboardType: TextInputType.number,
-          onChanged: (String value) {
-            setState(() {
-              priceValue = double.parse(value);
-            });
-          },
-        ),
-        Text(titleValue),
-      ],
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: const InputDecoration(
+              hintText: "Super Cake",
+              labelText: "Product Name",
+            ),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            decoration: const InputDecoration(
+              hintText: "This cake is build with heart",
+              labelText: "Product Description",
+            ),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: "19.99",
+              labelText: "Product Price",
+            ),
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+          Text(titleValue),
+        ],
+      ),
     );
   }
 }
