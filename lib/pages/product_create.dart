87cce17f871910099ef629a3e56035ed42579cba
Gaurav Gupta <gouravgupta840@gmail.com>
2024-06-10 10:01:30 +0530

    rafactor a bit more

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,47 +16,70 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _descriptionValue = '';
   bool _hasImage = false;
 
+  Widget _buildTitleTextField() {
+    return TextField(
+      decoration: const InputDecoration(
+        hintText: "Super Cake",
+        labelText: "Product Name",
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      maxLines: 4,
+      decoration: const InputDecoration(
+        hintText: "This cake is build with heart",
+        labelText: "Product Description",
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        hintText: "19.99",
+        labelText: "Product Price",
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+    );
+  }
+
+  void _submitForm() {
+    final Map<String, dynamic> product = {
+      'title': _titleValue,
+      'price': _priceValue,
+      'description': _descriptionValue,
+      'imageUrl': 'assets/food.jpg'
+    };
+    widget.addProduct(product);
+    Navigator.pushNamed(context, "/");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(10.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            decoration: const InputDecoration(
-              hintText: "Super Cake",
-              labelText: "Product Name",
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          ),
-          TextField(
-            maxLines: 4,
-            decoration: const InputDecoration(
-              hintText: "This cake is build with heart",
-              labelText: "Product Description",
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              hintText: "19.99",
-              labelText: "Product Price",
-            ),
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
           const SizedBox(
             height: 10,
           ),
@@ -69,18 +92,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
             },
             title: const Text("Has Picture"),
           ),
-          ElevatedButton(
-              onPressed: () {
-                final Map<String, dynamic> product = {
-                  'title': _titleValue,
-                  'price': _priceValue,
-                  'description': _descriptionValue,
-                  'imageUrl': 'assets/food.jpg'
-                };
-                widget.addProduct(product);
-                Navigator.pushNamed(context, "/");
-              },
-              child: const Text("Save")),
+          ElevatedButton(onPressed: _submitForm, child: const Text("Save")),
         ],
       ),
     );
