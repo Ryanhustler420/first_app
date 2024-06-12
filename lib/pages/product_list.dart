3ed2f40c1d89912561464ac9048ab8fc31595973
Gaurav Gupta <gouravgupta840@gmail.com>
2024-06-12 14:28:25 +0530

    improved list tile

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,30 +4,41 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
-  const ProductListPage({required this.products, required this.updateProduct, super.key});
+  const ProductListPage(
+      {required this.products, required this.updateProduct, super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Image.asset(products[index]['imageUrl']),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return ProductEditPage(
-                    product: products[index],
-                    addProduct: () {},
-                    productIndex: index,
-                    updateProduct: updateProduct,
-                  );
+        return Column(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(products[index]['imageUrl']),
+              ),
+              title: Text(products[index]['title']),
+              subtitle: Text(
+                "\$${products[index]['price'].toString()}",
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return ProductEditPage(
+                        product: products[index],
+                        addProduct: () {},
+                        productIndex: index,
+                        updateProduct: updateProduct,
+                      );
+                    },
+                  ));
                 },
-              ));
-            },
-          ),
+              ),
+            ),
+            const Divider()
+          ],
         );
       },
       itemCount: products.length,
